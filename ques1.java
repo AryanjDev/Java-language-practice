dc3c611258f2ef6aaef86a1f8c76a978878b2a96
AryanjDev <jsabita250@gmail.com>
2026-07-19 10:06:51 +0530

    First Question in git

--- ques1.java
+++ ques1.java
@@ -1,5 +1,24 @@
-package Java-language-practice;
+/*  Ques 1. Given a dynamic array A.
+Return a Dynamic Array that has all even numbers from A.
 
- {
+Note :
+Dynamic Array in JAVA: ArrayList
+Dynamic Array in C++: vector
+Dynamic Array in Python: List */
+import java.util.*;
+public class ques1 {
+        public static void main (String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            ArrayList<Integer> Anew = new ArrayList<>();
+            for(int i=0; i<n; i++){
+                
+            }
+
+
+        }
+    }
+
+
     
-}
+
